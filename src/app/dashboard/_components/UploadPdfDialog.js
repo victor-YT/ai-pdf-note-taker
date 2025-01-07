883806a02498fd2874ae2ff749ec18cb37d3db5d
victor-YT <143153852+victor-YT@users.noreply.github.com>
2025-01-07 19:48:16 +1000

    finish upload dialog

--- src/app/dashboard/_components/UploadPdfDialog.js
+++ src/app/dashboard/_components/UploadPdfDialog.js
@@ -2,11 +2,15 @@ import React from 'react'
 import {
     Dialog,
     DialogContent,
+    DialogClose,
+    DialogFooter,
     DialogDescription,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import {Input} from "@/components/ui/input"
+import {Button} from "@/components/ui/button"
 
 
 function UploadPdfDialog({children}) {
@@ -17,12 +21,28 @@ function UploadPdfDialog({children}) {
             </DialogTrigger>
             <DialogContent>
                 <DialogHeader>
-                    <DialogTitle>Are you absolutely sure?</DialogTitle>
+                    <DialogTitle>Upload PDF File</DialogTitle>
                     <DialogDescription>
-                        This action cannot be undone. This will permanently delete your account
-                        and remove your data from our servers.
+                        <div>
+                            <h2>Select a file to Upload</h2>
+                            <div className="mt-5 gap-2 p-3 rounded-md border">
+                                <input type='file'/>
+                            </div>
+                            <div className='mt-2'>
+                                <label>File Name *</label>
+                                <Input placeholder='File Name'/>
+                            </div>
+                        </div>
                     </DialogDescription>
                 </DialogHeader>
+                <DialogFooter className="sm:justify-end">
+                    <DialogClose asChild>
+                        <Button type="button" variant="secondary">
+                            Close
+                        </Button>
+                    </DialogClose>
+                    <Button>Upload</Button>
+                </DialogFooter>
             </DialogContent>
         </Dialog>
     )
