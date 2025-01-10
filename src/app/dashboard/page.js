2c686135aea04939793acb0017e20e8edea7fefc
victor-YT <143153852+victor-YT@users.noreply.github.com>
2025-01-10 10:19:37 +1000

    finish loading icon

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -14,31 +14,33 @@ function Dashboard() {
     console.log(fileList)
 
     return (
-        <div>
-            <h2 className='font-medium text-2xl'>workspace</h2>
-            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4
-            xl:grid-cols-5 gap-5 mt-8 bg-amber-500'>
-                {fileList?.length>0?fileList?.map((file, index) => (
-                    <div key={index} className='flex p-4 shadow-md rounded-md flex-col
-                    items-center justify-center border cursor-pointer hover:scale-105'>
-                        <Image src={'/pdf.png'} alt={'file'} width={50} height={50}/>
-                        <h2 className='mt-3 font-medium text-lg'>{file?.fileName}</h2>
-                    </div>
-                ))
-                    : <div>
+        fileList?.length > 0
+            ?
+            <div>
+                <h2 className='font-medium text-2xl'>workspace</h2>
+                <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4
+                xl:grid-cols-5 gap-5 mt-8'>
+                    {fileList?.map((file, index) => (
+                        <div key={index} className='flex p-4 shadow-md rounded-md flex-col
+                            items-center justify-center border cursor-pointer hover:scale-105'>
+                            <Image src={'/pdf.png'} alt={'file'} width={50} height={50}/>
+                            <h2 className='mt-3 font-medium text-lg'>{file?.fileName}</h2>
+                        </div>))}
+                </div>
+            </div>
+            :
+            <div className='flex flex-col h-[85vh] overflow-y-hidden'>
+                <h2 className='font-medium text-2xl'>workspace</h2>
+                <div className='flex-grow'>
+                    <div className="flex items-center justify-center h-full w-full">
                         <div className="loader">
-                            <div className="loader-square"></div>
-                            <div className="loader-square"></div>
-                            <div className="loader-square"></div>
-                            <div className="loader-square"></div>
-                            <div className="loader-square"></div>
-                            <div className="loader-square"></div>
-                            <div className="loader-square"></div>
+                            <span className="bar"></span>
+                            <span className="bar"></span>
+                            <span className="bar"></span>
                         </div>
                     </div>
-                }
+                </div>
             </div>
-        </div>
     )
 }
 
